4b9a9ad5203bc0b91c9a07073dbb2ef9e79a891b
ORC-1 <Rolandazim@gmail.com>
2019-02-11 17:55:53 +0100

    Beta release

---
+++ ibl_pip/setup.py
@@ -0,0 +1,60 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+from pkg_resources import DistributionNotFound
+
+import sys
+import os
+import glob
+import release
+#execfile('release.py')
+
+# setup params
+required_modules = ["setuptools"]
+#if mac, install readline
+#if(sys.platform=="darwin"):
+#    required_modules.append("readline >= 2.6.4")
+
+# nose is used for test
+extra_modules = {}
+
+setup(
+    name="ibolang",
+    version=release.version,
+    author=release.author,
+    author_email=release.email,
+    download_url=release.download_url,
+    license=license,
+    keywords = "traditional, simplified, Igbo, Afrocode, language, tokenize",
+    description=release.description,
+    long_description=release.long_description,
+    url=release.url,
+    zip_safe=False,
+    install_requires = required_modules,
+    extras_require = extra_modules,
+    include_package_data = True,
+    packages=find_packages(exclude=["ez_setup", 'examples', 'apidocs', "tests"]),
+    entry_points = """
+    [console_scripts]
+    ibolang = ibolang:commandline
+    
+    """,
+    classifiers = [
+        'Development Status :: Beta',
+        'Environment :: Console',
+        'Intended Audience :: Education',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Igbo (Traditional)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Code Generators'],
+    #test_suite = 'nose.collector',
+    )
+
