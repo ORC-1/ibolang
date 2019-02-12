3ce5a5eaf8e5642a16f4c47e49bc2e0f4a1c46b0
ORC-1 <Rolandazim@gmail.com>
2019-02-12 14:24:50 +0100

    Beta release

--- release.py
+++ release.py
@@ -3,7 +3,7 @@
 """Release information"""
 
 
-version = "1.0.0"
+version = "0.0.1"
 author = "Roland|Chima"
 email = "Rolandazim@gmail.com"
 copyright = "Copyright 2019~ Roland|Chima and contributors"
@@ -18,25 +18,49 @@ long_description = """
 Introduction
 --------------
 
-"Igbo is an indigenous language popularly spoken in Nigeria, Ibolang is a full 
-extension of the Igbo language in Python.
+Igbo is an indigenous language popularly spoken in Nigeria, Ibolang is a full 
+extension of the Igbo language in Python. 
 With Ibolang, you can write and run python like programs in Igbo
 
 Ibolang acts like python 3 and plays like python 3, it maintains all the python syntax 
 and methods.
 user could use it to learn programming in their native language.
 
-Check examples here.
+Example
+----------
+ibolang is highly user friendly, the following is a simple "HelloWorld" program
+       
+       deputa("Uwa Aloo")
 
-  * https://github.com/orc-1/ibolang/examples
+ 
+ running this, will diplay
+         
+         Uwa Aloo 
+
+to console, which translated to English is "Hello World"
+
+you can code more complex code by installing ibolang to your PC:
+
+
+To run programs is as simple as:
+
+     $ ibolang filename.ibl
+
+  from your preferred shell or command line  
+
+  you can go through the dictionary on: 
+  * https://github.com/ORC-1/ibolang/blob/master/dictionary.txt
+      
+  to get an exhaustive list of all currently available commands and there English translation
 
 Install
 ----------
 
-If you'd like to play Ibolang with full features, you should install Ibolang.
+If you'd like to play Ibolang with full features included, you should install Ibolang.
 
-You could use pip or easy_install command to install Ibolang::
-	$ pip install Ibolang 
+You could use pip or easy_install command to install Ibolang:
+
+	$ pip install Ibolang (available in 2 days)
 
 			or
 
@@ -51,14 +75,15 @@ And check your system path params if it contains python3.x/bin path.
 ex: edit .bashrc to include "/Library/Frameworks/Python.framework/Versions/3.x/bin" in your PATH parameter.
 
 For sytem running multiple version of python, you are better of using a virtual enviroment
-with Ibolang::
+with Ibolang:
+
 	$ conda create -n Ibolang python==3.XX
 
 			or using Virtualenv
 
 	$ virtualenv ibolang python==3.XX 
 
-Lastly you can clone the repo using this url: navigate to the folder path and run python setup.py 
+Lastly you can clone the repo using this url https://github.com/ORC-1/ibolang.git : navigate to the folder path and run python setup.py 
 Copy the source files into your script folder, you should highly consider using 
 a virtual enviroment if you are using this option and the previous options are better 
 off
@@ -70,6 +95,7 @@ Change Log
 
 You could view the ChangeLog to see what's new in these version.
 
-  * http://code.google.com/p/zhpy/source/browse/CHANGELOG.txt
+  * https://github.com/ORC-1/ibolang/blob/master/CHANGELOG.txt
+
 
 """
